25c9c753c6facd1b8517929421d752ed1f883b37
Tom <tom@debian>
2017-05-26 14:23:35 +0200

    Added the variable resolve code

--- var2names_pretty/README.md
+++ var2names_pretty/README.md
@@ -1,3 +1,6 @@
 ## var2name_pretty
 This is a module has the same function as the var2name module but its prints a 
-pretty format of the status while the names gets resolved
+pretty format of the status while the names gets resolved.
+
+You can step through the process variable by variable and see how the matching
+is done.
--- var2names_pretty/main.c
+++ var2names_pretty/main.c
@@ -18,31 +18,124 @@
 
 #define BOX_WIDTH (strlen(NAME)+MAX_VARNAME_LENGTH+4)
 
+vars_t *g_lda_anker = NULL, *g_nat_anker = NULL;
+int error_num, error;
+
+vars_t *resolveName(vars_t *nat_set, vars_t *lda_set, vars_t *var_anker);
 int main()
 {
-    vars_t *lda_anker = NULL, *nat_anker = NULL;
     int ret;
 
     char error_buff[2024];
 
-    initVarAnker(&nat_anker);
-    initVarAnker(&lda_anker);
+    initVarAnker(&g_nat_anker);
+    initVarAnker(&g_lda_anker);
 
-    addString(nat_anker, NULL, "var1", L"val1", 5);
-    addString(nat_anker, NULL, "var2", L"val2", 5);
-    addString(nat_anker, NULL, "var3", L"val3", 5);
-    addString(nat_anker, NULL, "var4", L"val4", 5);
+    addString(g_nat_anker, NULL, "var1", L"val", 5);
+    addString(g_nat_anker, NULL, "var2", L"val", 5);
+    addString(g_nat_anker, NULL, "var3", L"val", 5);
+    addString(g_nat_anker, NULL, "var4", L"val", 5);
+    addString(g_nat_anker, NULL, "var5", L"val", 5);
+    addString(g_nat_anker, NULL, "var6", L"val", 5);
+    addString(g_nat_anker, NULL, "var7", L"val", 5);
 
-    if((ret = startLDAParser("./TEST.NGL", lda_anker, NULL, error_buff)) != LDA_OK)
+    if((ret = startLDAParser("./TEST.NGL", g_lda_anker, NULL, error_buff)) != LDA_OK)
     {
         printf("Something went wrong [%s]\n", error_buff);
         return(-1);
     }
 
-    printfork(lda_anker, stdout);
-
-    p_print(lda_anker, nat_anker, NULL, NULL);
+    //printfork(g_lda_anker, stdout);
+    
+    resolveName(g_nat_anker, g_lda_anker, g_nat_anker);
 
     return(0);
 }
 
+vars_t *resolveName(vars_t *nat_set, vars_t *lda_set, vars_t *var_anker)
+{
+    vars_t *cur_lda_set = lda_set,
+           *cur_nat_set = nat_set;
+    bool in_grp = false;
+    char *grp_name = NULL;
+    int ret = 0;
+
+    if(cur_lda_set->type == GROUP)
+    {
+        if(GrpisDefined(var_anker, cur_lda_set->name) == false)
+        {
+            if((ret = addGroup(var_anker, cur_lda_set->name, 0,0,0)) != 0)
+            {
+                error_num = ret;
+                error = RH4N_VAR_LIBRARY_ERR;
+                return(NULL);
+            }
+        }
+        in_grp = true;
+        grp_name = cur_lda_set->name;
+        cur_lda_set = cur_lda_set->next_lvl;
+    }
+
+    while(cur_lda_set && cur_nat_set)
+    {
+        if(cur_lda_set->type == GROUP)
+        {
+            cur_nat_set = resolveName(cur_nat_set, cur_lda_set, var_anker);
+            if(error != 0)
+                return(NULL);
+        }
+        else if(cur_lda_set->type == REDEFINE)
+        {
+            cur_lda_set = cur_lda_set->next;
+            continue;
+        }
+        else
+        {
+            free(cur_nat_set->name);
+            if((cur_nat_set->name = malloc(strlen(cur_lda_set->name)+1)) == NULL)
+            {
+                error = RH4N_NO_MEMORY;
+                return(NULL);
+            }
+            strcpy(cur_nat_set->name, cur_lda_set->name);
+            p_print(g_lda_anker, g_nat_anker, cur_lda_set, cur_nat_set);
+            printf("Enter to continue...");
+            getchar();
+            printf("================================================================================\n");
+
+            if(in_grp == true)
+            {            
+                if((ret = moveVariableToGroup(var_anker, cur_nat_set->name, grp_name, &cur_nat_set)) != 0)
+                {
+                    error_num = ret;
+                    error = RH4N_VAR_LIBRARY_ERR;
+                    return(NULL);
+                }
+            }
+            else
+            {
+                cur_nat_set = cur_nat_set->next;
+            }
+        }
+        cur_lda_set = cur_lda_set->next;
+    }
+    return(cur_nat_set);
+}
+
+vars_t *searchPageStructure(vars_t *lda_anker)
+{
+    vars_t *hptr = lda_anker->next;
+
+    char *tmp = NULL;
+
+    while(hptr)
+    {
+        if(strcmp(str_toLower(hptr->name), RH4N_GRP_HEAD) == 0)
+        {
+            return(hptr);
+        }
+        hptr = hptr->next;
+    }
+
+    return(NULL);
+}
