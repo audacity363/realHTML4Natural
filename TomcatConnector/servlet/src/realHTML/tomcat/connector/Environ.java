dca20880dc7f8b7820668e9fbb0acba614f3f122
Tom Engemann <tom@engemann.me>
2017-11-14 12:15:15 +0100

    You can now set enviroment variables in the enviroment which gets set in the natural process

---
+++ TomcatConnector/servlet/src/realHTML/tomcat/connector/Environ.java
@@ -0,0 +1,11 @@
+package realHTML.tomcat.connector;
+
+public class Environ {
+    public String name;
+    public String value;
+    public boolean append;
+
+    public Environ() {
+        this.append = false;
+    }
+}
