0ab10f1e51e48c64fe941fb21b1c5d169077fd76
Tom Engemann <tom@engemann.me>
2018-02-23 17:32:05 +0100

    Removed json generator and varhandle lib
    In preparation for a new varhandle library

--- libs/varhandle_lib/src/group.h
+++
@@ -1,4 +0,0 @@
-bool GrpisDefined(vars_t *anker, char *name);
-int moveVariableToGroup(vars_t *anker, char *var_name, char *grp_name, vars_t **next_entry);
-int addGroup(vars_t *anker, char *name, int x_length, int y_length, int z_length);
-vars_t *getGrpPointer(vars_t *anker, char *name);
